3bf5c66db893ca4b8dd5d42fdb4c4fa446cf908c
Peter Grønbæk Andersen <peter@grnbk.io>
2025-11-04 06:54:21 +0100

    chore: require windows in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
+        'pywin32; platform_system=="Windows"'
     ],
     author="Peter Grønbæk Andersen",
     author_email="peter@grnbk.io",
@@ -12,14 +13,14 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     project_urls={
-        "Homepage": "https://github.com/pgroenbaek/pytkutils",
-        "Issues": "https://github.com/pgroenbaek/pytkutils/issues",
-        "Source": "https://github.com/pgroenbaek/pytkutils",
+        "Homepage": "https://github.com/pgroenbaek/pyffeditc",
+        "Issues": "https://github.com/pgroenbaek/pyffeditc/issues",
+        "Source": "https://github.com/pgroenbaek/pyffeditc",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
     ],
     python_requires=">=3.6",
 )
