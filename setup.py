05aa5ddd0e15529e352e2add757513cf90c94f36
Peter Grønbæk Andersen <peter@grnbk.io>
2025-11-27 09:48:50 +0100

    chore: increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyffeditc",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         'pywin32; platform_system=="Windows"'
