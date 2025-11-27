c15b7ce857a2ba9cd39cfd8800260ba5b98afa42
Peter Grønbæk Andersen <peter@grnbk.io>
2025-11-27 09:15:25 +0100

    fix: use full path to executable rather than rely upon cwd as that does not always work on windows

--- pyffeditc/wrapper.py
+++ pyffeditc/wrapper.py
@@ -40,20 +40,19 @@ def compress(input_path: str, output_path: str, ffeditc_exe_path: str) -> bool:
     if not os.path.exists(input_path):
         raise FileNotFoundError(f"No such file or directory: '{input_path}")
     
-    output_directory = os.path.dirname(output_path)
+    output_dir = os.path.dirname(output_path)
 
-    if not os.path.isdir(output_directory):
-        raise FileNotFoundError(f"No such file or directory: '{output_directory}")
+    if not os.path.isdir(output_dir):
+        raise FileNotFoundError(f"No such file or directory: '{output_dir}")
 
     if not os.path.exists(ffeditc_exe_path):
         raise FileNotFoundError(f"No such file or directory: '{ffeditc_exe_path}")
 
     executable_dir = os.path.dirname(ffeditc_exe_path)
-    executable_name = os.path.basename(ffeditc_exe_path)
 
     try:
         result = subprocess.run(
-            [executable_name, input_path, "/c", "/o:" + output_path],
+            [ffeditc_exe_path, input_path, "/c", "/o:" + output_path],
             cwd=executable_dir,
             capture_output=True,
             text=True,
@@ -94,11 +93,10 @@ def decompress(input_path: str, output_path: str, ffeditc_exe_path: str) -> bool
         raise FileNotFoundError(f"No such file or directory: '{ffeditc_exe_path}")
 
     executable_dir = os.path.dirname(ffeditc_exe_path)
-    executable_name = os.path.basename(ffeditc_exe_path)
 
     try:
         result = subprocess.run(
-            [executable_name, input_path, "/u", "/o:" + output_path],
+            [ffeditc_exe_path, input_path, "/u", "/o:" + output_path],
             cwd=executable_dir,
             capture_output=True,
             text=True,
